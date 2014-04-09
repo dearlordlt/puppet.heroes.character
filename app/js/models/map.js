7bae4ea0644b6576a73f6f6890ba3285fe0d8b9f
dearlordlt <sarunas79@gmail.com>
2014-04-09 18:28:01 +0300

    Model reworked

--- app/js/models/map.js
+++ app/js/models/map.js
@@ -1,3 +1,16 @@
+/* Old way
 var map_tpl = {
     title : "Map"
 }
+*/
+
+/* New Way */
+var model_map_tpl = function(title, size) {
+    this.title = title;
+    this.size = size;
+}
+
+var map_tpl = new model_map_tpl(
+    "Map",
+    0
+);
