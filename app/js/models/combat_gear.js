92c97e6ff1881a1efcf5d2b16d47b1c18926c47e
Butkus <andbut@gmail.com>
2014-04-09 20:45:20 +0300

    tear added

--- app/js/models/combat_gear.js
+++ app/js/models/combat_gear.js
@@ -1,7 +1,9 @@
-var model_cg_tpl = function(title) {
+var model_cg_tpl = function(title, tear) {
     this.title = title;
+    this.tear = tear;
 }
 
 var cg_tpl = new model_cg_tpl(
-    "Combat Gear"
+    "Combat Gear",
+    "Tear"
 );
