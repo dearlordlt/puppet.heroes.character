ce7cfd6f3b1741c470e612a59aa414bbe0c82fb9
dearlordlt <sarunas79@gmail.com>
2014-04-10 10:41:58 +0300

    Map model

--- app/js/models/map.js
+++ app/js/models/map.js
@@ -1,9 +1,11 @@
-var model_map_tpl = function(title, size) {
+var model_map_tpl = function(title, sizeX, sizeY) {
     this.title = title;
-    this.size = size;
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
 }
 
 var map_tpl = new model_map_tpl(
-    "Map",
-    0
+    "Map", /* Title */
+    10, /* sizeX */
+    10 /* sizeY */
 );
