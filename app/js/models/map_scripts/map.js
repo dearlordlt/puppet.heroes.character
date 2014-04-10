fa3639aec73b89a89aaf137a3f1a896a86f23dea
dearlordlt <sarunas79@gmail.com>
2014-04-10 11:40:14 +0300

    Map draws canvas dynamicaly, then draws some stupid hexagon

---
+++ app/js/models/map_scripts/map.js
@@ -0,0 +1,15 @@
+var canvas = document.createElement('canvas');
+canvas.id     = "CursorLayer";
+canvas.width  = 600;
+canvas.height = 600;
+//canvas.style.zIndex   = 8;
+//canvas.style.position = "absolute";
+canvas.style.border   = "1px solid";
+
+mapCanvas = document.getElementById("mapCanvas");
+mapCanvas.appendChild(canvas);
+
+paper.setup(canvas);
+
+var hexagon = new paper.Path.RegularPolygon(new paper.Point(200, 200), 6, 100);
+hexagon.strokeColor = '#ff0000';
