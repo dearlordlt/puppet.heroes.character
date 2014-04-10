57585dad0eb71c5ad4f8c4d7bfcef2293968986e
Butkus <andbut@gmail.com>
2014-04-10 21:56:24 +0300

    Attribute data model created

--- app/js/models/combat_gear.js
+++ app/js/models/combat_gear.js
@@ -1,9 +1,16 @@
-var model_cg_tpl = function(title, tear) {
+var model_cg_tpl = function(title, Attributes) {
     this.title = title;
-    this.tear = tear;
+    this.Attributes = Attributes;
 }
 
 var cg_tpl = new model_cg_tpl(
     "Combat Gear",
-    "Tear"
+    [
+        [{attributeName : "Strength"},  {str : 10}],
+        [{attributeName : "Stamina"},   {sta : 10}],
+        [{attributeName : "Dexterity"}, {dex : 10}],
+        [{attributeName : "Reflex"},    {ref : 10}],
+        [{attributeName : "Perseption"},{per : 10}],
+        [{attributeName : "Will"},      {wil : 10}]
+    ]
 );
