2df94fac2d3b3baf9f5843b70e541aada7e2ce79
dearlordlt <sarunas79@gmail.com>
2014-04-10 13:38:21 +0300

    Added comments to helper file

--- app/js/helper.js
+++ app/js/helper.js
@@ -1,7 +1,27 @@
+/**
+ * Global functions and constants goes here
+ */
+
+/**
+ * Checks is given argument is valid number
+ * @param {Type} n
+ */
 function isNumber(n) {
    return n == parseFloat(n);
 }
 
+/**
+ * Checks is given argument is even
+ * @param {Type} n
+ */
 function isEven(n) {
    return isNumber(n) && (n % 2 == 0);
+}
+
+/**
+ * Checks is given argument is odd
+ * @param {Type} n
+ */
+function isOdd(n) {
+   return isNumber(n) && (Math.abs(n) % 2 == 1);
 }
