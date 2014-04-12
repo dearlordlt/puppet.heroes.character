0b07229b0b77c6025831f50c0fcc9ebe250da563
Butkus <andbut@gmail.com>
2014-04-12 14:40:23 +0300

    Added hide parts function to Combat Gear template

--- app/js/models/cg_scripts/cg_script.js
+++ app/js/models/cg_scripts/cg_script.js
@@ -1,13 +1,7 @@
+
 $("fieldset legend").click(function() {
-  if ($(this).parent().children().length == 2)
-    $(this).parent().find("div").toggle();
-  else
-  {
-    $(this).parent().wrapInner("<div>");
-    $(this).appendTo($(this).parent().parent());
-    $(this).parent().find("div").toggle();
-  }
-});
+                $(this).siblings('.hidingParts').toggle();
+        });
 
 $("button[id^=attrMinusButton]").click(function() {
     var arrayPlace = this.id.match(/\d+/);
