7bf6f473ef08eb22437f7f497f847a571d151546
dearlordlt <sarunas79@gmail.com>
2014-04-12 18:08:26 +0300

    Resize to items added

--- app/js/models/dnd_scripts/dnd_script.js
+++ app/js/models/dnd_scripts/dnd_script.js
@@ -3,12 +3,12 @@ var gridster;
 $(function() {
     gridster = $(".gridster ul").gridster({
         widget_base_dimensions: [
-            parseInt($(".gridster").width())/2,
+            300,
             100],
         widget_margins: [5, 5],
         helper: 'clone',
         resize: {
-            enabled: false,
+            enabled: true,
             max_size: [2, 2],
             min_size: [1, 1]
         }
