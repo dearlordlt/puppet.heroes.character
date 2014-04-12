465603959b767b528a21a03c0138f48d4a6a41cc
dearlordlt <sarunas79@gmail.com>
2014-04-12 15:46:41 +0300

    Base files added. Dummy template ccreated

---
+++ app/js/models/dnd.js
@@ -0,0 +1,9 @@
+var model_dnd_tpl = function (title, description) {
+    this.title = title;
+    this.description = description;
+}
+
+var dnd_tpl = new model_dnd_tpl (
+    "Drag and Drop example.",
+    "Use it at your own risk!"
+);
