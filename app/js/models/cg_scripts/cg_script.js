cbee6f6f4a4cd11ef3de1717212c13950b6c9acd
Anbut <andbut@gmail.com>
2014-04-14 02:17:55 +0300

    Cleaned up scrip

--- app/js/models/cg_scripts/cg_script.js
+++ app/js/models/cg_scripts/cg_script.js
@@ -1,11 +1,11 @@
 var gridster;
 
-$(function() {
+$(function () {
     gridster = $(".cgGridster").gridster({
         widget_base_dimensions: [
             300,
-            150],
-        widget_margins: [0, 0],
+            100],
+        widget_margins: [5, 5],
         helper: 'clone',
         resize: {
             enabled: true,
@@ -16,40 +16,39 @@ $(function() {
 });
 
 
-$("button[id^=attrMinusButton]").click(function() {
-    var arrayPlace = this.id.match(/\d+/);
-    var _validatesmall = cg_tpl.Attributes[arrayPlace].attributeValue - 1;
-    var _points = cg_tpl.points;
+$("button[id^=attrMinusButton]").click(function () {
+    var arrayPlace = this.id.match(/\d+/),
+        _validatesmall = cg_tpl.Attributes[arrayPlace].attributeValue - 1,
+        _points = cg_tpl.points;
 
-        if(cg_tpl.Attributes[arrayPlace].attributeValue > 10 ) {
-            cg_tpl.points -= (Math.ceil((((cg_tpl.Attributes[arrayPlace].attributeValue-10)/3)* 10) / 10))*10
-        }
-
-
-   if (_validatesmall <= 7) {
-     cg_tpl.Attributes[arrayPlace].attributeValue = 7;
-    } else {
-     cg_tpl.Attributes[arrayPlace].attributeValue -= 1;
+    if (cg_tpl.Attributes[arrayPlace].attributeValue > 10) {
+        cg_tpl.points -= (Math.ceil((((cg_tpl.Attributes[arrayPlace].attributeValue - 10) / 3) * 10) / 10)) * 10;
     }
 
-        if(cg_tpl.Attributes[arrayPlace].attributeValue < 10 ) {
-            cg_tpl.points += (Math.floor((((cg_tpl.Attributes[arrayPlace].attributeValue-10)/3)* 10) / 10))*10
-        }
+
+    if (_validatesmall <= 7) {
+        cg_tpl.Attributes[arrayPlace].attributeValue = 7;
+    } else {
+        cg_tpl.Attributes[arrayPlace].attributeValue -= 1;
+    }
+
+    if (cg_tpl.Attributes[arrayPlace].attributeValue < 10) {
+        cg_tpl.points += (Math.floor((((cg_tpl.Attributes[arrayPlace].attributeValue - 10) / 3) * 10) / 10)) * 10;
+    }
 
     loadPath("cg_tpl");
     refreshPoints("cg_tpl");
-    $("#collapseOne"+arrayPlace).addClass("panel-collapse collapse in");
+    $("#collapseOne" + arrayPlace).addClass("panel-collapse collapse in");
 });
 
 
-$("button[id^=attrPlusButton]").click(function(){
-    var arrayPlace = this.id.match(/\d+/);
-    var _validatesmall = cg_tpl.Attributes[arrayPlace].attributeValue + 1;
+$("button[id^=attrPlusButton]").click(function () {
+    var arrayPlace = this.id.match(/\d+/),
+        _validatesmall = cg_tpl.Attributes[arrayPlace].attributeValue + 1;
 
-        if(cg_tpl.Attributes[arrayPlace].attributeValue < 10 ) {
-            cg_tpl.points -= (Math.floor((((cg_tpl.Attributes[arrayPlace].attributeValue-10)/3)* 10) / 10))*10
-        }
-
+    if (cg_tpl.Attributes[arrayPlace].attributeValue < 10) {
+        cg_tpl.points -= (Math.floor((((cg_tpl.Attributes[arrayPlace].attributeValue - 10) / 3) * 10) / 10)) * 10;
+    }
 
     if (_validatesmall >= 20) {
         cg_tpl.Attributes[arrayPlace].attributeValue = 20;
@@ -58,13 +57,13 @@ $("button[id^=attrPlusButton]").click(function(){
     }
 
 
-        if(cg_tpl.Attributes[arrayPlace].attributeValue > 10 ) {
-           cg_tpl.points += (Math.ceil((((cg_tpl.Attributes[arrayPlace].attributeValue-10)/3)* 10) / 10))*10
-        }
+    if (cg_tpl.Attributes[arrayPlace].attributeValue > 10) {
+        cg_tpl.points += (Math.ceil((((cg_tpl.Attributes[arrayPlace].attributeValue - 10) / 3) * 10) / 10)) * 10;
+    }
 
-     loadPath("cg_tpl");
-     refreshPoints("cg_tpl");
-    $("#collapseOne"+arrayPlace).addClass("panel-collapse collapse in");
+    loadPath("cg_tpl");
+    refreshPoints("cg_tpl");
+    $("#collapseOne" + arrayPlace).addClass("panel-collapse collapse in");
 });
 
 
