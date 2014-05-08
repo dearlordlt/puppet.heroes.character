71f1e6f2d4b4040a08c4a041dc118963a0041178
dearlordlt <sarunas79@gmail.com>
2014-05-08 16:08:39 +0300

    test added

--- app/js/models/header_scripts/header_script.js
+++ app/js/models/header_scripts/header_script.js
@@ -1,9 +1,17 @@
 if(!supports_html5_storage()) {
     $("#actions-menu").hide();
-    phAlert("System alert", "Local storage is not supported by your system. Try to use different browser");
-
+    phAlert(
+        "System alert",
+        "Local storage is not supported by your system. Try to use different browser"
+    );
+} else {
     //Check if user already have data setup
-
+    if(localStorage["puppet.hero.signed"] == "true") {
+        console.log("Already user");
+    } else {
+        localStorage["puppet.hero.signed"] = "true"
+        console.log("Not user");
+    }
 }
 
 //Make stuff draggable
