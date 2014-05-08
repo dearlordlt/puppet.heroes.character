94dadf8315529a097b23269dec77bb32194d8790
dearlordlt <sarunas79@gmail.com>
2014-05-08 16:08:39 +0300

    Local storage not supported alert added

--- app/js/models/header_scripts/header_script.js
+++ app/js/models/header_scripts/header_script.js
@@ -1,3 +1,8 @@
+if(!supports_html5_storage()) {
+    $("#actions-menu").hide();
+    phAlert("System alert", "Local storage is not supported by your system. Try to use different browser");
+}
+
 $("#global-alert").draggable();
 $("#global-alert button").click(function () {
     $("#global-alert").toggle(200);
