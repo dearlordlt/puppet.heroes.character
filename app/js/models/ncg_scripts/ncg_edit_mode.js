e78d8affaa917238c44bfce1ddb64cd61342a96f
Daumantas Girininkas <nim@nim.lt>
2014-05-08 17:35:03 +0300

    validation corrections

--- app/js/models/ncg_scripts/ncg_edit_mode.js
+++ app/js/models/ncg_scripts/ncg_edit_mode.js
@@ -1,8 +1,6 @@
-//
 $("#edit-mode-btn").click(function() {
     $(".edit-mode-item").toggle();
 });
-
 
 $("#ncg-character-edit").hide();
 
@@ -19,16 +17,43 @@ $(".ncg-character-info").each(function (index) {
     });
 });
 
-
 $("#ncg-character-btn-save").click(function () {
+
+
+
+    var _originalAttr = $("#ncg-character-edit").attr("class");
+    var _originalBtnAttr = $("#ncg-character-btn-save").attr("class");
     var _characterItemValue = $("#ncg-character-input").val();
     var _characterItemKey = $("#ncg-character-input").data("key");
-    if (_characterItemKey == "Age" && !isNumber(_characterItemValue)) return;
-    if (_characterItemValue.length < 2 || _characterItemValue.length > 30) return;
-    ncg_tpl.character[_characterItemKey] = _characterItemValue;
-    loadPath("ncg_tpl");
+    if (_characterItemKey == "Age") {
+        if (!isNumber(_characterItemValue)) {
+            $("#ncg-character-edit").attr('class', "has-error " + _originalAttr);
+            $("#ncg-character-btn-save").attr('class', "btn-danger " + _originalBtnAttr);
+            return;
+        }
+        if (parseInt(_characterItemValue) < 0) {
+            $("#ncg-character-edit").attr('class', "has-error " + _originalAttr);
+            $("#ncg-character-btn-save").attr('class', "btn-danger " + _originalBtnAttr);
+            return;
+        }
+        if (_characterItemValue.length > 5) {
+            $("#ncg-character-edit").attr('class', "has-error " + _originalAttr);
+            $("#ncg-character-btn-save").attr('class', "btn-danger " + _originalBtnAttr);
+            return;
+        }
+        ncg_tpl.character[_characterItemKey] = _characterItemValue;
+        loadPath("ncg_tpl");
+    }
+    else {
+        if (_characterItemValue.length < 2 || _characterItemValue.length > 30) {
+            $("#ncg-character-edit").attr('class', "has-error " + _originalAttr);
+            $("#ncg-character-btn-save").attr('class', "btn-danger " + _originalBtnAttr);
+            return;
+        }
+        ncg_tpl.character[_characterItemKey] = _characterItemValue;
+        loadPath("ncg_tpl");
+    }
 });
-
 
 $("#ncg-character-btn-cancel").click(function() {
     $("#ncg-character-edit").hide();
