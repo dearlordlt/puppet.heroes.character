6f2d0ab3d727f285ea2f045952100df55da97227
dearlordlt <sarunas79@gmail.com>
2014-05-09 12:38:04 +0300

    Character loads from list now

--- app/js/models/header_scripts/header_script.js
+++ app/js/models/header_scripts/header_script.js
@@ -10,10 +10,11 @@ if(!supports_html5_storage()) {
 } else {
     //Check if user already have data setup
     if(localStorage["puppet.hero.signed"] == "true") {
-        //phAlert("Local storage", "Already user");
+        //
     } else {
         localStorage["puppet.hero.signed"] = "true"
-        phAlert("Howdy", "No local data found<br>Created new entry.");
+        var _characters = {characters : []};
+        localStorage["puppet.hero.@.characters"] = JSON.stringify(_characters);
     }
 }
 
@@ -51,24 +52,54 @@ $("#menu-save").click(function () {
 
 $("#menu-load").click(function () {
     $("#global-load-name").show(200);
+    //Fill load characters list #load-character-name
+    var _characters = JSON.parse(localStorage["puppet.hero.@.characters"]);
+    $("#load-character-name").html("");
+    for(var i = 0; i < _characters.characters.length; i ++) {
+        $("#load-character-name").append('<option value="' + _characters.characters[i] + '">' + _characters.characters[i] + '</option>');
+    }
+
     $("#load-as-name").val(ncg_tpl.character.Name);
 });
 
 $("#save-character-button").click(function() {
-    localStorage["puppet.hero.#."+ncg_tpl.character.Name+".cg"] = JSON.stringify(cg_tpl);
-    localStorage["puppet.hero.#."+ncg_tpl.character.Name+".ncg"] = JSON.stringify(ncg_tpl);
-    localStorage["puppet.hero.#."+ncg_tpl.character.Name+".map"] = JSON.stringify(map_tpl);
+    var _cName = $("#save-as-name").val();
+
+    localStorage["puppet.hero.#."+_cName+".cg"] = JSON.stringify(cg_tpl);
+    localStorage["puppet.hero.#."+_cName+".ncg"] = JSON.stringify(ncg_tpl);
+    localStorage["puppet.hero.#."+_cName+".map"] = JSON.stringify(map_tpl);
+
+    var _characters = {characters : []};
+
+    try {
+        _characters = JSON.parse(localStorage["puppet.hero.@.characters"]);
+        _characters.characters.push(_cName);
+        localStorage["puppet.hero.@.characters"] = JSON.stringify(_characters);
+    }
+    catch(_err) {
+        phAlert("Data alert", "No data found: " + _err + " " + _characters);
+    }
 
     $("#global-save-name").hide(200);
 });
 
 $("#load-character-button").click(function() {
-    cg_tpl = JSON.parse(localStorage["puppet.hero.#."+$("#load-as-name").val()+".cg"]);
-    ncg_tpl = JSON.parse(localStorage["puppet.hero.#."+$("#load-as-name").val()+".ncg"]);
-    map_tpl = JSON.parse(localStorage["puppet.hero.#."+$("#load-as-name").val()+".map"]);
+    var _cName = $("#load-character-name option:selected").text();
+
+    try {
+        JSON.parse(localStorage["puppet.hero.#." + _cName + ".cg"]);
+    }
+    catch(_err) {
+        phAlert("Warning", "No character named " + _cName + " found");
+    }
+
+    cg_tpl = JSON.parse(localStorage["puppet.hero.#." + _cName + ".cg"]);
+    ncg_tpl = JSON.parse(localStorage["puppet.hero.#." + _cName + ".ncg"]);
+    map_tpl = JSON.parse(localStorage["puppet.hero.#." + _cName + ".map"]);
 
     $("#global-load-name").hide(200);
     $("#main-menu").show(200);
     $("#menu-save").show();
+
     loadPath("home_tpl");
 });
