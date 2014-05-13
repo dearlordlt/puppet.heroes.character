2ac4f65d57c7012bdf24e45fed970e46a0519b43
dearlordlt <sarunas79@gmail.com>
2014-05-13 13:30:39 +0300

    Server side logic added

--- server/server.js
+++ server/server.js
@@ -10,3 +10,21 @@ function user(username, password, email) {
 }
 
 var user1 = new user("saras", "saras++", "sarunas79@gmail.com");
+
+/*
+db.users.save(user1, function(err, savedUser) {
+    if(err || !savedUser) {
+        console.log("User " + user1.email + " not saved because of error " + err);
+    } else {
+        console.log("User " + savedUser.email + "saved");
+    }
+});
+*/
+
+db.users.find(user1, function(err, users) {
+    if(err || !users.length) console.log("User " + user.email + " not found");
+    // if some users are found
+    else users.forEach(function (user) {
+        console.log('User Found! - ' + user);
+    });
+})
